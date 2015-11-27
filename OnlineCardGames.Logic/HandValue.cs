bc94a9dd02ee6e43147ada506dddb517f38a2512
Matthew Ellwood <Seaal@users.noreply.github.com>
2015-11-27 23:14:56 +0000

    Added tests for getting a hand value for a hand

---
+++ OnlineCardGames.Logic/HandValue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineCardGames.Logic
+{
+    public class HandValue
+    {
+        public enum HandType
+        {
+            HighCard = 0,
+            Pair = 1,
+            TwoPair = 2,
+            ThreeOfAKind = 3,
+            Straight = 4,
+            Flush = 5,
+            FullHouse = 6,
+            FourOfAKind = 7,
+            StraightFlush = 8,
+            RoyalFlush = 9
+        }
+
+        public HandType Type { get; set; }
+        public int[] Kickers { get; set; }
+
+        public HandValue(HandType type, params int[] kickers)
+        {
+            if (kickers.Length != 5)
+            {
+                throw new InvalidOperationException("Must provide 5 kickers");
+            }
+
+            Type = type;
+            Kickers = kickers;
+        }
+
+        private int GetValue()
+        {
+            int result = ((int)Type) * 10 ^ 6;
+
+            for (int i = 0; i < Kickers.Length; i++)
+            {
+                result += Kickers[i] * 10 ^ (Kickers.Length - i);
+            }
+
+            return result;
+        }
+
+        public static bool operator >(HandValue left, HandValue right)
+        {
+            return left.GetValue() > right.GetValue();
+        }
+
+        public static bool operator <(HandValue left, HandValue right)
+        {
+            return left.GetValue() < right.GetValue();
+        }
+    }
+}
---
+++ OnlineCardGames.Tests/PokerLogic/HandValueServiceTests.cs
@@ -0,0 +1,262 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using OnlineCardGames.Entities;
+using OnlineCardGames.Logic;
+
+namespace OnlineCardGames.Tests.PokerLogic
+{
+    [TestFixture]
+    public class HandValueServiceTests
+    {
+        [Test]
+        public void Should_Return_Royal_Flush()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 14, Suit = Suit.Clubs },
+                new Card() { Number = 13, Suit = Suit.Clubs }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 4, Suit = Suit.Diamonds },
+                new Card() { Number = 10, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 12, Suit = Suit.Clubs },
+                new Card() { Number = 11, Suit = Suit.Clubs}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.RoyalFlush, 14, 13, 12, 11, 10));
+        }
+
+        [Test]
+        public void Should_Return_Straight_Flush()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 10, Suit = Suit.Hearts },
+                new Card() { Number = 6, Suit = Suit.Hearts }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 9, Suit = Suit.Hearts },
+                new Card() { Number = 14, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 8, Suit = Suit.Hearts },
+                new Card() { Number = 3, Suit = Suit.Clubs}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.StraightFlush, 10, 9, 8, 7, 6));
+        }
+
+        [Test]
+        public void Should_Return_4_Of_A_Kind()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 7, Suit = Suit.Clubs },
+                new Card() { Number = 10, Suit = Suit.Diamonds }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 10, Suit = Suit.Hearts },
+                new Card() { Number = 10, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 12, Suit = Suit.Clubs },
+                new Card() { Number = 10, Suit = Suit.Spades}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.FourOfAKind, 10, 10, 10, 10, 12));
+        }
+
+        [Test]
+        public void Should_Return_Full_House()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 5, Suit = Suit.Hearts },
+                new Card() { Number = 2, Suit = Suit.Diamonds }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 10, Suit = Suit.Hearts },
+                new Card() { Number = 2, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 7, Suit = Suit.Clubs },
+                new Card() { Number = 2, Suit = Suit.Spades}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.FourOfAKind, 2, 2, 2, 7, 7));
+        }
+
+        [Test]
+        public void Should_Return_Flush()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 6, Suit = Suit.Spades },
+                new Card() { Number = 7, Suit = Suit.Spades }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 12, Suit = Suit.Spades },
+                new Card() { Number = 2, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 9, Suit = Suit.Spades },
+                new Card() { Number = 2, Suit = Suit.Spades}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.Flush, 12, 9, 7, 6, 2));
+        }
+
+        [Test]
+        public void Should_Return_Straight()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 4, Suit = Suit.Diamonds },
+                new Card() { Number = 8, Suit = Suit.Spades }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 6, Suit = Suit.Spades },
+                new Card() { Number = 13, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 5, Suit = Suit.Clubs },
+                new Card() { Number = 3, Suit = Suit.Spades}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.Straight, 8, 7, 6, 5, 4));
+        }
+
+        [Test]
+        public void Should_Return_Three_Of_A_Kind()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 12, Suit = Suit.Spades },
+                new Card() { Number = 14, Suit = Suit.Spades }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 6, Suit = Suit.Spades },
+                new Card() { Number = 14, Suit = Suit.Clubs },
+                new Card() { Number = 7, Suit = Suit.Hearts },
+                new Card() { Number = 14, Suit = Suit.Hearts },
+                new Card() { Number = 5, Suit = Suit.Diamonds}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.ThreeOfAKind, 14, 14, 14, 12, 7));
+        }
+
+        [Test]
+        public void Should_Return_Two_Pair()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 3, Suit = Suit.Clubs },
+                new Card() { Number = 5, Suit = Suit.Spades }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 6, Suit = Suit.Spades },
+                new Card() { Number = 9, Suit = Suit.Clubs },
+                new Card() { Number = 3, Suit = Suit.Hearts },
+                new Card() { Number = 11, Suit = Suit.Hearts },
+                new Card() { Number = 5, Suit = Suit.Diamonds}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.TwoPair, 5, 5, 3, 3, 11));
+        }
+
+        [Test]
+        public void Should_Return_Pair()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 9, Suit = Suit.Diamonds },
+                new Card() { Number = 10, Suit = Suit.Diamonds }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 6, Suit = Suit.Spades },
+                new Card() { Number = 14, Suit = Suit.Clubs },
+                new Card() { Number = 2, Suit = Suit.Hearts },
+                new Card() { Number = 2, Suit = Suit.Clubs },
+                new Card() { Number = 11, Suit = Suit.Diamonds}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.Pair, 2, 2, 14, 11, 10));
+        }
+
+        [Test]
+        public void Should_Return_High_Card()
+        {
+            HandValueService handValueService = new HandValueService();
+
+            List<Card> playerCards = new List<Card>()
+            {
+                new Card() { Number = 9, Suit = Suit.Diamonds },
+                new Card() { Number = 10, Suit = Suit.Diamonds }
+            };
+
+            List<Card> board = new List<Card>()
+            {
+                new Card() { Number = 6, Suit = Suit.Spades },
+                new Card() { Number = 14, Suit = Suit.Clubs },
+                new Card() { Number = 2, Suit = Suit.Hearts },
+                new Card() { Number = 4, Suit = Suit.Clubs },
+                new Card() { Number = 11, Suit = Suit.Diamonds}
+            };
+
+            HandValue handValue = handValueService.CalculateHandValue(playerCards, board);
+
+            handValue.Should().BeSameAs(new HandValue(HandValue.HandType.HighCard, 14, 11, 10, 9, 6));
+        }
+    }
+}
