6009a08bb4802f5e772cd3110dd602a27dc66b73
Matthew Ellwood <Seaal@users.noreply.github.com>
2015-11-28 11:23:08 +0000

    Fixed login using email instead of username

--- OnlineCardGames/Models/AccountViewModels.cs
+++ OnlineCardGames/Models/AccountViewModels.cs
@@ -54,9 +54,8 @@ namespace OnlineCardGames.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
